41385511cc1affd85ba659ba35dcd39df6f43c81
hitaishi2222 <hitaishi2222@gmail.com>
2024-12-10 20:16:48 +0530

    added my_module

---
+++ Docs/cl/my_module.py
@@ -0,0 +1,14 @@
+def addition(a, b):
+    print(a + b)
+
+
+def subtraction(a, b):
+    return print(a - b)
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
---
+++ Docs/cl/operations.py
@@ -0,0 +1,3 @@
+from my_module import addition, subtraction, multiply, divide
+
+print(addition(2, 5))
